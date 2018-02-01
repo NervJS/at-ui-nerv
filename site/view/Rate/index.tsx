e20aedb1fc3c458e9856526db36b1c29810ffd11
李天惠 <litianhui@jd.com>
2018-02-01 15:59:23 +0800

    feat: add rate component & test complete

---
+++ site/view/Rate/index.tsx
@@ -0,0 +1,82 @@
+import * as Nerv from 'nervjs'
+
+import Rate from '../../../src/rate'
+
+interface RateExampleState {
+  value: number
+  value2: number
+}
+
+class Rate_example extends Nerv.Component<null, RateExampleState> {
+  constructor (...args) {
+    super(...args)
+    this.state = {
+      value: 0,
+      value2: 3.5
+    }
+  }
+  onChangeHandle = (index: number) => {
+    this.setState({
+      value: index
+    })
+  }
+  halfStarOnChange = (index) => {
+    this.setState({
+      value2: index
+    })
+  }
+  onHoverChange = (index) => {
+    console.log(index)
+  }
+  render () {
+    const { value, value2 } = this.state
+    return (
+      <div className='rate_example'>
+        <div className='basic'>
+          <Rate />
+          <Rate count={6}/>
+        </div>
+        <div className='hasText'>
+          <Rate showText={true} value={value} onChange={this.onChangeHandle}>
+            <span>{value} 星</span>
+          </Rate>
+        </div>
+        <div className='different_icon'>
+          <Rate icon='icon-heart-on' />
+        </div>
+        <div className='halfStar'>
+          <Rate allowHalf={true} showText={true} value={value2} onHoverChange={this.onHoverChange} onChange={this.halfStarOnChange}>
+            <span> {value2} 星</span>
+          </Rate>
+        </div>
+        <div className='disabled'>
+          <Rate
+            allowHalf={true}
+            showText={true}
+            disabled={true}
+            value={value2}
+            onChange={this.halfStarOnChange}
+          />
+        </div>
+        <div className='allowClear'>
+          <Rate
+            allowHalf={true}
+            allowClear={true}
+            showText={true}
+            value={value2}
+            onChange={this.halfStarOnChange}
+          />
+          <Rate
+            allowHalf={true}
+            allowClear={false}
+            showText={true}
+            value={value2}
+            onChange={this.halfStarOnChange}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Rate_example
---
+++ src/rate/__test__/index.test.tsx
@@ -0,0 +1,66 @@
+import * as Nerv from 'nervjs'
+import * as sinon from 'sinon'
+import * as $ from 'webpack-zepto'
+
+import Rate from '../rate'
+
+describe('InputNumber test', () => {
+  let scratch
+  beforeAll(() => {
+    scratch = document.createElement('div')
+    document.body.appendChild(scratch)
+  })
+
+  beforeEach(() => {
+    scratch = document.createElement('div')
+    document.body.appendChild(scratch)
+  })
+
+  afterAll(() => {
+    scratch.parentNode.removeChild(scratch)
+    scratch = null
+  })
+
+  it('basic render', () => {
+    const rate = <Rate />
+    const component = Nerv.render(rate, scratch)
+    expect($(component.dom).find('.at-rate__item').length).toBeTruthy()
+  })
+  it('should render text with showText props', () => {
+    const rate = <Rate showText={true} />
+    const component = Nerv.render(rate, scratch)
+    expect($(component.dom).find('.at-rate__text').length).toBe(1)
+  })
+  it('rendered icon type should be equal to icon props ', () => {
+    const rate = <Rate icon={'icon-heart-on'} />
+    const component = Nerv.render(rate, scratch)
+    expect(
+      $(component.dom)
+        .find('.icon')
+        .hasClass('icon-heart-on')
+    ).toBeTruthy()
+  })
+  it('half star', () => {
+    const value = 3.5
+    const rate = <Rate value={value} allowHalf={true} />
+    const component = Nerv.render(rate, scratch)
+    const idx = Math.floor(value)
+    expect($(component.dom).find('.at-rate__item').eq(idx).hasClass('at-rate__item--half')).toBeTruthy()
+  })
+  it('disabled', () => {
+    const onChangeHandle = sinon.spy()
+    const rate = <Rate disabled={true} onChange={onChangeHandle}/>
+    const component = Nerv.render(rate, scratch)
+    $(component.dom).find('.at-rate__icon').trigger('click')
+    expect(onChangeHandle.calledOnce).not.toBeTruthy()
+  })
+  it('allowClear', (done) => {
+    const onChangeHandle = (val) => {
+      expect(val).toBe(0)
+      done()
+    }
+    const rate = <Rate value={3} onChange={onChangeHandle} allowClear={true}/>
+    const component = Nerv.render(rate, scratch)
+    $(component.dom).find('.at-rate__icon').eq(2).trigger('click')
+  })
+})
