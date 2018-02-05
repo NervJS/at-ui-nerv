3acf96f1261471028b5b927d6c1182f7e3f48b1f
李天惠 <litianhui@jd.com>
2018-02-05 11:32:36 +0800

    test: add badge test
    add badge component test case and 100% pass through

--- src/badge/__test__/index.test.tsx
+++ src/badge/__test__/index.test.tsx
@@ -1,7 +1,7 @@
 import * as Nerv from 'nervjs'
 import * as $ from 'webpack-zepto'
 import * as sinon from 'sinon'
-import { Checkbox, CheckboxGroup } from '../'
+import Badge from '../'
 
 describe('Checkbox', () => {
   let scratch
@@ -19,4 +19,46 @@ describe('Checkbox', () => {
     scratch.parentNode.removeChild(scratch)
     scratch = null
   })
+  it('basic render', () => {
+    for (let value = -20; value < 20; value++) {
+      const badge = <Badge value={value} />
+      const component = Nerv.render(badge, scratch)
+      expect(
+        Number(
+          $(component.dom)
+            .find('.at-badge__content')
+            .text()
+        )
+      ).toBe(value)
+    }
+  })
+  it('textContent render', () => {
+    const value = 'test'
+    const badge = <Badge value={value} />
+    const component = Nerv.render(badge, scratch)
+    expect(
+      $(component.dom)
+        .find('.at-badge__content')
+        .text()
+    ).toBe(value)
+  })
+  it('different type should have different Color', () => {
+    const statusList = ['error', 'success', 'warning', 'info']
+    statusList.forEach((status) => {
+      const badge = <Badge status={status} value={123} />
+      const component = Nerv.render(badge, scratch)
+      expect($(component.dom).hasClass(`at-badge--${status}`)).toBeTruthy()
+    })
+  })
+  it('over max num limited should render with + ', () => {
+    const badge = <Badge value={100} maxNum={99} />
+    const component = Nerv.render(badge, scratch)
+    expect($(component.dom).text()).toBe('99+')
+  })
+  it('dot render', () => {
+    const badge = <Badge value={100} maxNum={99} dot/>
+    const component = Nerv.render(badge, scratch)
+    expect($(component.dom).find('.at-badge--dot').length).toBe(1)
+  })
+
 })
