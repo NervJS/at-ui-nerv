5c2185612f048f6fafce1b7ca7af5ec6cde0133a
李天惠 <litianhui@jd.com>
2018-02-05 14:54:49 +0800

    feat: add card component & test complete
    finish card component and test suite development

---
+++ src/card/__test__/index.test.tsx
@@ -0,0 +1,55 @@
+import * as Nerv from 'nervjs'
+import * as $ from 'webpack-zepto'
+import * as sinon from 'sinon'
+import Card from '../'
+
+describe('card', () => {
+  let scratch
+  beforeAll(() => {
+    scratch = document.createElement('div')
+    document.body.appendChild(scratch)
+  })
+
+  beforeEach(() => {
+    scratch = document.createElement('div')
+    document.body.appendChild(scratch)
+  })
+
+  afterAll(() => {
+    scratch.parentNode.removeChild(scratch)
+    scratch = null
+  })
+  it('basic render', () => {
+    const card = (
+      <Card>
+        <div slot='title'>title</div>
+        <div slot='extra'>
+          <a>extra</a>
+        </div>
+        <div>content</div>
+      </Card>
+    )
+    const component = Nerv.render(card, scratch)
+    expect(
+      $(component.dom)
+        .find('.at-card__title')
+        .text()
+    ).toBe('title')
+    expect(
+      $(component.dom)
+        .find('.at-card__extra')
+        .text()
+    ).toBe('extra')
+    expect(
+      $(component.dom)
+        .find('.at-card__body')
+        .html()
+    ).toBe('<div>content</div>')
+  })
+  it('bodyStyle test', () => {
+    const bodyStyle = { padding: 0 }
+    const card = <Card style={{ width: '300px' }} bodyStyle={bodyStyle} />
+    const component = Nerv.render(card, scratch)
+    expect($(component.dom).find('.at-card__body').css('padding')).toBe('0px')
+  })
+})
