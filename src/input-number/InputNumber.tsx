b57bedb9ae80090b26ac61bb539c5d77f9716b91
kyjo2014 <467346087@qq.com>
2018-04-19 14:02:45 +0800

    fix: fixed inputNumber init fail
    currentVal is 0 as prev edition , it will cause component failure when min props > 0

--- src/input-number/InputNumber.tsx
+++ src/input-number/InputNumber.tsx
@@ -28,33 +28,46 @@ class InputNumber extends Nerv.Component<InputNumberProps, InputNumberState> {
     min: 0,
     max: Number.POSITIVE_INFINITY
   }
-  timeout: number
+  timeout: any
   constructor (...args) {
     super(...args)
     this.state = {
       currentValue: this.props.value || 0
     }
   }
+  componentDidMount () {
+    const { min } = this.props
+    if (min) {
+      this.setState({
+        currentValue: Number(min)
+      })
+    }
+  }
   increaseNum = () => {
     const { currentValue } = this.state
     const { max = Number.POSITIVE_INFINITY, disabled } = this.props
     const value = currentValue || 0
+
     if (value >= max || disabled) {
       return
     }
     this.calculateStep('up')
   }
   decreaseNum = () => {
-    const {currentValue} = this.state
-    const {min = Number.NEGATIVE_INFINITY, disabled} = this.props
+    const { currentValue } = this.state
+    const { min = Number.NEGATIVE_INFINITY, disabled } = this.props
     const value = currentValue || 0
-    if (value <= min || disabled) { return }
+    if (value <= min || disabled) {
+      return
+    }
     this.calculateStep('down')
   }
   calculateStep = (type) => {
     const { currentValue } = this.state
     const { max = Number.POSITIVE_INFINITY, min = Number.NEGATIVE_INFINITY, disabled, step, onChange } = this.props
-    if (disabled) { return }
+    if (disabled) {
+      return
+    }
     let value = Number(currentValue)
     const stepNum = Number(step)
     if (type === 'up') {
@@ -62,10 +75,13 @@ class InputNumber extends Nerv.Component<InputNumberProps, InputNumberState> {
     } else if (type === 'down') {
       value = this.calculateNumber(value, stepNum, '-')
     }
-
-    if (value > max || value < min) { return }
+    if (value > max || value < min) {
+      return
+    }
     this.setState({ currentValue: value })
-    onChange(value)
+    if (onChange instanceof Function) {
+      onChange(value)
+    }
   }
   calculateNumber = (num: number, stepNum: number, symbol: string): number => {
     let decimal1, decimal2
@@ -84,9 +100,9 @@ class InputNumber extends Nerv.Component<InputNumberProps, InputNumberState> {
 
     const quantity = Math.pow(10, Math.max(decimal1, decimal2))
     if (symbol === '+') {
-      return ((num * quantity) + (stepNum * quantity)) / quantity
+      return (num * quantity + stepNum * quantity) / quantity
     } else if (symbol === '-') {
-      return ((num * quantity) - (stepNum * quantity)) / quantity
+      return (num * quantity - stepNum * quantity) / quantity
     } else {
       return num
     }
@@ -97,18 +113,23 @@ class InputNumber extends Nerv.Component<InputNumberProps, InputNumberState> {
       onBlur()
     }
     if (evt.target instanceof HTMLInputElement) {
-      const {min = Number.NEGATIVE_INFINITY , max = Number.POSITIVE_INFINITY, onChange} = this.props
+      const { min = Number.NEGATIVE_INFINITY, max = Number.POSITIVE_INFINITY, onChange } = this.props
       let val = Number(evt.target.value)
       if (val > max) {
         val = max
       } else if (val < min) {
         val = min
       }
-      this.setState({
-        currentValue: val
-      }, () => {
-        onChange(val)
-      })
+      this.setState(
+        {
+          currentValue: val
+        },
+        () => {
+          if (onChange instanceof Function) {
+            onChange(val)
+          }
+        }
+      )
     }
   }
   onFocusHandle = (evt: CompositeComponent): void => {
@@ -132,18 +153,30 @@ class InputNumber extends Nerv.Component<InputNumberProps, InputNumberState> {
   onChangeHandle = (evt) => {
     if (evt.target instanceof HTMLInputElement) {
       const val = Number(evt.target.value)
-      this.setState({
-        currentValue: val
-      }, () => {
-        clearTimeout(this.timeout)
-        this.timeout = setTimeout(() => {
-          this.onBlurHandle(evt)
-        }, 750)
-      })
+      this.setState(
+        {
+          currentValue: val
+        },
+        () => {
+          clearTimeout(this.timeout)
+          this.timeout = setTimeout(() => {
+            this.onBlurHandle(evt)
+          }, 750)
+        }
+      )
     }
   }
   render () {
-    const { size, disabled, name, readonly, max = Number.POSITIVE_INFINITY, min = Number.NEGATIVE_INFINITY, autofocus, step } = this.props
+    const {
+      size,
+      disabled,
+      name,
+      readonly,
+      max = Number.POSITIVE_INFINITY,
+      min = Number.NEGATIVE_INFINITY,
+      autofocus,
+      step
+    } = this.props
     const { currentValue } = this.state
     const upDisabled = currentValue + Number(step) > max
     const downDisabled = currentValue - Number(step) < min
@@ -152,8 +185,7 @@ class InputNumber extends Nerv.Component<InputNumberProps, InputNumberState> {
         className={classnames('at-input-number', {
           [`at-input-number--${size}`]: !!size,
           'at-input-number--disabled': disabled
-        })}
-      >
+        })}>
         <div className='at-input-number__input'>
           <input
             type='number'
@@ -177,16 +209,14 @@ class InputNumber extends Nerv.Component<InputNumberProps, InputNumberState> {
             className={classnames('at-input-number__up', {
               'at-input-number__up--disabled': upDisabled
             })}
-            onClick={this.increaseNum}
-          >
+            onClick={this.increaseNum}>
             <i className='icon icon-chevron-up' />
           </span>
           <span
             className={classnames('at-input-number__down', {
               'at-input-number__down--disabled': downDisabled
             })}
-            onClick={this.decreaseNum}
-          >
+            onClick={this.decreaseNum}>
             <i className='icon icon-chevron-down' />
           </span>
         </div>
