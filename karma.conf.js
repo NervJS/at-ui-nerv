88655beea7bb9545b67a53f19de860792406186f
李天惠 <litianhui@jd.com>
2018-07-03 20:04:21 +0800

    build: add .jsx loader to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -115,7 +115,7 @@ module.exports = function (config) {
         rules: [
           {
             enforce: 'pre',
-            test: /\.js$/,
+            test: /\.(js|jsx)$/,
             loader: 'babel-loader',
             exclude: /node_modules/
           },
