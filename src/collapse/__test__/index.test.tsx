7c67984f5970ccaf2d64151efbb2ea2e435d5df6
litianhui <litianhui1@jd.com>
2018-07-05 16:34:34 +0800

    test: fixed collapse test case

--- src/collapse/__test__/index.test.tsx
+++ src/collapse/__test__/index.test.tsx
@@ -1,45 +1,41 @@
 import * as Nerv from 'nervjs'
 import * as $ from 'webpack-zepto'
 import * as sinon from 'sinon'
-import { } from 'nerv-test-utils'
 import Collapse from '../'
 
 describe('Collapse', () => {
   let scratch
   beforeAll(() => {
     scratch = document.createElement('div')
-    document
-      .body
-      .appendChild(scratch)
+    document.body.appendChild(scratch)
   })
 
   beforeEach(() => {
     scratch = document.createElement('div')
-    document
-      .body
-      .appendChild(scratch)
+    document.body.appendChild(scratch)
   })
 
   afterAll(() => {
-    scratch
-      .parentNode
-      .removeChild(scratch)
+    scratch.parentNode.removeChild(scratch)
     scratch = null
   })
 
   it('basic render', (done) => {
     const onChange = sinon.spy()
-    const collapseJSX = <Collapse onChange={onChange}>
-      {}
-      <Collapse.Item key={'test'}>
-      {}
-        <div>test</div>
-      </Collapse.Item>
-    </Collapse>
+    const collapseJSX = (
+      <Collapse onChange={onChange}>
+        {}
+        <Collapse.Item key={'test'}>
+          {}
+          <div>test</div>
+        </Collapse.Item>
+      </Collapse>
+    )
     const component = Nerv.render(collapseJSX as any, scratch)
     expect($(component.vnode.dom).find('.at-collapse')).toBeDefined()
     $(component.vnode.dom)
-      .find('.at-collapse__header').trigger('click')
+      .find('.at-collapse__header')
+      .trigger('click')
     Nerv.nextTick(() => {
       expect(onChange.callCount).toBe(1)
       done()
@@ -47,49 +43,73 @@ describe('Collapse', () => {
   })
   it('accordion state', (done) => {
     const onChange = sinon.spy()
-    const accordionJSX = <Collapse accordion onChange={onChange}>
-     {}
-      <Collapse.Item key={'test'} title='title'>
-      {}
-        <div>test</div>
-      </Collapse.Item>
-      <Collapse.Item key={'test2'}>
+    const accordionJSX = (
+      <Collapse accordion onChange={onChange}>
         {}
-        <div>test</div>
-      </Collapse.Item>
-    </Collapse>
+        <Collapse.Item title='title'>
+          {}
+          <div>test</div>
+        </Collapse.Item>
+        <Collapse.Item>
+          {}
+          <div>test</div>
+        </Collapse.Item>
+      </Collapse>
+    )
     const component = Nerv.render(accordionJSX as any, scratch)
     $(component.vnode.dom)
       .find('.at-collapse__header')
       .eq(0)
       .trigger('click')
-    $(component.vnode.dom)
-      .find('.at-collapse__header')
-      .eq(1)
-      .trigger('click')
-    Nerv.nextTick(() => {
-      expect($(component.vnode.dom).find('.at-collapse__item').eq(0).hasClass('at-collapse__item--active')).toBeFalsy()
-      expect($(component.vnode.dom).find('.at-collapse__item').eq(1).hasClass('at-collapse__item--active')).toBeTruthy()
-      done()
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        $(component.vnode.dom)
+          .find('.at-collapse__header')
+          .eq(1)
+          .trigger('click')
+        resolve()
+      }, 1000)
+    }).then(() => {
+      Nerv.nextTick(() => {
+        expect(onChange.callCount).toBe(
+          2,
+          'switch twice should call callback same times'
+        )
+        expect(
+          $(component.vnode.dom)
+            .find('.at-collapse__item')
+            .eq(0)
+            .hasClass('at-collapse__item--active')
+        ).toBeFalsy()
+        expect($(component.vnode.dom).find('.at-collapse__item').eq(1)
+          .hasClass('at-collapse__item--active')).toBeTruthy()
+        done()
+      })
     })
   })
   it('nest collapse', (done) => {
     const onChange = sinon.spy()
-    const nestJSX = <Collapse onChange={onChange}>
-    {}
-      <Collapse.Item>
-      {}
-        <Collapse accordion>
+    const nestJSX = (
+      <Collapse onChange={onChange}>
         {}
-          <Collapse.Item title='嵌套面板'>
+        <Collapse.Item>
           {}
-            <div>嵌套面板的内容</div>
-          </Collapse.Item>
-        </Collapse>
-      </Collapse.Item>
-    </Collapse>
+          <Collapse accordion>
+            {}
+            <Collapse.Item title='嵌套面板'>
+              {}
+              <div>嵌套面板的内容</div>
+            </Collapse.Item>
+          </Collapse>
+        </Collapse.Item>
+      </Collapse>
+    )
     const component = Nerv.render(nestJSX as any, scratch)
-    expect($(component.vnode.dom).find('.at-collapse__content').children('.at-collapse').length).toBe(1)
+    expect(
+      $(component.vnode.dom)
+        .find('.at-collapse__content')
+        .children('.at-collapse').length
+    ).toBe(1)
     done()
   })
 })
