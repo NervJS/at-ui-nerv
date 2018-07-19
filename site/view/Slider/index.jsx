016ac8c4b1095183acad4cb9c2942877fb330497
litianhui <litianhui1@jd.com>
2018-07-19 09:48:41 +0800

    docs(slider): add slider docs

--- site/view/Slider/index.jsx
+++ site/view/Slider/index.jsx
@@ -1,6 +1,6 @@
 import * as Nerv from 'nervjs'
 
-import Slider from '../../../src/slider'
+import Slider from '@md/slider.md'
 
 class SliderExample extends Nerv.Component {
   constructor (props) {
@@ -11,19 +11,10 @@ class SliderExample extends Nerv.Component {
   }
   render () {
     return (
-      <div className='Slider-example'>
-        <Slider onChange={this.handleChange} />
-        <div style='height:100px;' />
-        <Slider disabled onChange={this.handleChange} value={20} />
-        <div style='height:100px;' />
-        <Slider onChange={this.handleChange} max={90} min={10} value={3} />
-        <div style='height:100px;' />
-        <Slider onChange={this.handleChange} step={3} />
+      <div className='slider-example'>
+        <Slider />
       </div>
     )
-  }
-  handleChange (value) {
-    console.log(value)
   }
 }
 
