23a2a76fccc98d86c25e73e10d39b5158a03dd23
litianhui <litianhui1@jd.com>
2018-07-23 10:44:24 +0800

    test(timeline): add timeline test

--- src/timeline/__test__/index.test.tsx
+++ src/timeline/__test__/index.test.tsx
@@ -1,13 +1,118 @@
 import * as Nerv from 'nervjs'
-import { renderIntoDocument } from 'nerv-test-utils'
-import Icon from '../'
+import $ from 'webpack-zepto'
+import Timeline from '../'
+import { VNode } from 'nerv-shared'
 
-describe('Icon', () => {
-  it('render Icon', () => {
-    const icon = <Icon type='icon-cloud'>{}Icon</Icon>
-    const component = renderIntoDocument(icon)
-    const dom = Nerv.findDOMNode(component)
-    // expect(component.props).toHaveProperty('type', 'icon-cloud')
-    expect(dom.textContent).toEqual('Icon')
+describe('timeline test', () => {
+  let scratch
+  beforeAll(() => {
+    scratch = document.createElement('div')
+    document.body.appendChild(scratch)
+  })
+
+  beforeEach(() => {
+    scratch = document.createElement('div')
+    document.body.appendChild(scratch)
+  })
+
+  afterEach(() => {
+    scratch.parentNode.removeChild(scratch)
+    scratch = null
+  })
+  it('basic render', () => {
+    const timelineJSX = (
+      <Timeline>
+        {}
+        <Timeline.Item>
+          {}
+          <p>刷牙洗脸</p>
+        </Timeline.Item>
+        <Timeline.Item>
+          {}
+          <p>吃早餐</p>
+        </Timeline.Item>
+        <Timeline.Item>
+          {}
+          <p>上班</p>
+        </Timeline.Item>
+        <Timeline.Item>
+          {}
+          <p>睡觉</p>
+        </Timeline.Item>
+      </Timeline>
+    )
+    const component = Nerv.render(timelineJSX as VNode, scratch)
+    const dom = $(Nerv.findDOMNode(component))
+    expect(dom.hasClass('at-timeline')).toBeTruthy()
+    expect(dom.find('.at-timeline__item').length).toBe(4)
+  })
+  it('color property should show status', () => {
+    const colors = {
+      blue: 'at-timeline__item--default',
+      green: 'at-timeline__item--success',
+      red: 'at-timeline__item--error',
+      yellow: 'at-timeline__item--warning'
+    }
+    Object.keys(colors).forEach((color) => {
+      const timelineJSX = (
+        <Timeline>
+          {}
+          <Timeline.Item color={color}>
+            {}
+            <p>刷牙洗脸</p>
+          </Timeline.Item>
+        </Timeline>
+      )
+      const component = Nerv.render(timelineJSX as VNode, scratch)
+      const dom = $(Nerv.findDOMNode(component))
+      expect(dom.find(`.${colors[color]}`).length).toBe(1)
+    })
+  })
+  it('pending property should render ghost point', () => {
+    const timelineJSX = (
+      <Timeline pending>
+        {}
+        <Timeline.Item>
+          {}
+          <p>刷牙洗脸</p>
+        </Timeline.Item>
+        <Timeline.Item>
+          {}
+          <p>吃早餐</p>
+        </Timeline.Item>
+        <Timeline.Item>
+          {}
+          <p>上班</p>
+        </Timeline.Item>
+        <Timeline.Item>
+          {}
+          <p>睡觉</p>
+        </Timeline.Item>
+      </Timeline>
+    )
+    const component = Nerv.render(timelineJSX as VNode, scratch)
+    const dom = $(Nerv.findDOMNode(component))
+    expect(
+      dom
+        .find('.at-timeline__item ')
+        .eq(-1)
+        .hasClass('at-timeline__item--pending')
+    ).toBeTruthy()
+  })
+  it('custom slot', () => {
+    const timelineJSX = (
+      <Timeline>
+        {}
+        <Timeline.Item color='blue'>
+          {}
+          <i slot='dot' className='icon icon-github' />
+          <p>刷牙洗脸</p>
+        </Timeline.Item>
+      </Timeline>
+    )
+    const component = Nerv.render(timelineJSX as VNode, scratch)
+    const dom = $(Nerv.findDOMNode(component))
+    expect(dom.find('.at-timeline__dot').html()).toEqual('<i slot="dot" class="icon icon-github"></i>')
+    expect(dom.find('.at-timeline__content').html()).toEqual('<p>刷牙洗脸</p>')
   })
 })
